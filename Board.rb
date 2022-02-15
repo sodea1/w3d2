88387fc6f4ca3b3bd58200fa62bf65a61ef4dc98
App Academy Student <email@email.com>
2022-02-15 12:15:54 -0500

    commit derek

--- Board.rb
+++ Board.rb
@@ -1,8 +1,24 @@
 class Board
 
-  ALPHABET = ("a".."z").to_a
   def initialize
-    @grid=Array.new(4){Array.new(4)}
+    @grid=Array.new(2){Array.new(2)}
+    @alphabet = ("A".."Z").to_a
+  end
+
+  def populate
+    while @grid.flatten.include?(nil)
+      random_letter = @alphabet.sample
+      while @grid.flatten.count(random_letter) < 2
+        row = rand(0...@grid.length)
+        col = rand(0...@grid[0].length)
+        @grid[row][col]=random_letter if @grid[row][col].nil?
+      end
+    end
+    # @grid
+  end
+
+  def render
+    
   end
 
 
